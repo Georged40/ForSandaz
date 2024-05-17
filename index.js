318ce2469b26e3b03b3a3ff8d72e5d13e99b6cd6
Georged40 <93541179+Georged40@users.noreply.github.com>
2024-05-17 17:23:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const forSurvival = document.getElementById('for-survival')
+
+
+
+closeModal.addEventListener("click", function(){
+   document.getElementById('modalWrap').style.display = "block;"
+})
+
+// function reveal(){
+  // document.getElementById('modalWrap').style.display = "block";
+}
+
+forSurvival.addEventListener("click", function(){
+  document.getElementById('modalWrap').style.display = "none";
+})
